ccc7435d8112544a1ce4be4ad7c830b5776fe5f3
jweah555 <j_weah@uncg.edu>
2025-06-03 00:18:34 -0400

    Added api folder and created database

---
+++ backend-api/src/test/java/com/example/Game_Platform/GamePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Game_Platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

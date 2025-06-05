ba4e8f55523064af50c0b2732e4c33928becd1af
jweah555 <j_weah@uncg.edu>
2025-06-05 06:11:31 -0400

    Almost completed Game folder

---
+++ backend-api/src/main/java/com/example/Game_Platform/Game/GameRepository.java
@@ -0,0 +1,17 @@
+package com.example.Game_Platform.Game;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, Long>{
+    
+    List<Game> getGameByGameName(String name);
+
+    @Query(value = "select * from games c where c.game_library_id = ?1", nativeQuery = true)
+    List<Game> getGamesByGameLibraryId(Long gameLibraryId);
+
+}

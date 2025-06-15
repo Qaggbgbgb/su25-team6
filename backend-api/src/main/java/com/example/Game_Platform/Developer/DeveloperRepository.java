6859187a61d2ba6bd35d2ba9c339e3827a3bf855
jweah555 <j_weah@uncg.edu>
2025-06-15 15:00:48 -0400

    uncommented

--- backend-api/src/main/java/com/example/Game_Platform/Developer/DeveloperRepository.java
+++ backend-api/src/main/java/com/example/Game_Platform/Developer/DeveloperRepository.java
@@ -1,22 +1,22 @@
-// package com.example.Game_Platform.Developer;
+package com.example.Game_Platform.Developer;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.data.jpa.repository.Query;
-// import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-// import com.example.Game_Platform.Game.Game;
-// @Repository
-// public interface DeveloperRepository extends JpaRepository<Developer, Long>{
-
-
-// @Query(value = "select * from Developers s where s.name like %?1% ", nativeQuery = true)
-//     List<Developer> getDevelopersByUsername(String username);
+import com.example.Game_Platform.Game.Game;
+@Repository
+public interface DeveloperRepository extends JpaRepository<Developer, Long>{
 
 
-// @Query(value = "select * from gameStores r where r.storeId= ?1", nativeQuery = true)
-//     List<Developer> getDevelopersByStoreId(Long storeId);
-// }
+@Query(value = "select * from Developers s where s.name like %?1% ", nativeQuery = true)
+    List<Developer> getDevelopersByUsername(String username);
+
+
+@Query(value = "select * from gameStores r where r.storeId= ?1", nativeQuery = true)
+    List<Developer> getDevelopersByStoreId(Long storeId);
+}
 
 

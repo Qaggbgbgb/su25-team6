fdb413edb1c186a85318f2418793c937be2b53b0
jweah555 <j_weah@uncg.edu>
2025-06-16 10:19:38 -0400

    Converted Chat RestAPI to Controller, mostly complete with chat

--- backend-api/src/main/java/com/example/Game_Platform/Developer/developer.java
+++ backend-api/src/main/java/com/example/Game_Platform/Developer/developer.java
@@ -1,86 +1,86 @@
-// package com.example.Game_Platform.Developer;
+package com.example.Game_Platform.Developer;
 
-// import com.example.Game_Platform.GameStore.GameStore;
-// import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.example.Game_Platform.GameStore.GameStore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-// import jakarta.persistence.Column;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.JoinColumn;
-// import jakarta.persistence.ManyToOne;
-// import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
-// @Entity
-// @Table(name = "Developers")
-// public class Developer {
+@Entity
+@Table(name = "Developers")
+public class Developer {
 
-// @Id
-// @GeneratedValue(strategy = GenerationType.IDENTITY)
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
 
-// private Long developer_id;
-// @Column(nullable = false, unique=true)
+private Long developer_id;
+@Column(nullable = false, unique=true)
 
-// private String username;
-// @Column(nullable = false)
-// private String password;
-
-// @ManyToOne()
-//   @JoinColumn(name = "storeId", nullable = false)
-//   @JsonIgnoreProperties("Developers")
-//   private GameStore gameStore;
+private String username;
+@Column(nullable = false)
+private String password;
 
 @ManyToOne()
   @JoinColumn(name = "storeId", nullable = false)
   @JsonIgnoreProperties("Developers")
   private GameStore gameStore;
 
-// public Developer(){
+@ManyToOne()
+  @JoinColumn(name = "storeId", nullable = false)
+  @JsonIgnoreProperties("Developers")
+  private GameStore gameStore;
 
-// }
-// public Developer(Long developer_id, String username, String password)
-// {
-//     this.developer_id=developer_id;
-//     this.username=username;
-//     this.password=password;
-// }
-// public Developer(String username, String password)
-// {
-//     this.username=username;
-//     this.password=password;
-// }
-// public Long getDeveloperID(){
-//     return this.developer_id;
-// }
+public Developer(){
 
-// public void setDeveloperID(Long developer_id)
-// {
-//     this.developer_id= developer_id;
-// }
+}
+public Developer(Long developer_id, String username, String password)
+{
+    this.developer_id=developer_id;
+    this.username=username;
+    this.password=password;
+}
+public Developer(String username, String password)
+{
+    this.username=username;
+    this.password=password;
+}
+public Long getDeveloperID(){
+    return this.developer_id;
+}
 
-// public String getUsername(){
-//     return this.username;
-// }
+public void setDeveloperID(Long developer_id)
+{
+    this.developer_id= developer_id;
+}
 
-// public void setUsername(String username)
-// {
-//     this.username=username;
-// }
+public String getUsername(){
+    return this.username;
+}
 
-// public String getPassword(){
-//     return this.password;
-// }
+public void setUsername(String username)
+{
+    this.username=username;
+}
 
-// public void setPassword(String password){
-//     this.password=password;
-// }
+public String getPassword(){
+    return this.password;
+}
 
-// public GameStore getGameStore(){
-//     return this.gameStore;
-// }
-// public void setGameStore(GameStore gameStore){
-//     this.gameStore=gameStore;
-// }
+public void setPassword(String password){
+    this.password=password;
+}
 
-// }
+public GameStore getGameStore(){
+    return this.gameStore;
+}
+public void setGameStore(GameStore gameStore){
+    this.gameStore=gameStore;
+}
+
+}

7ab69509192133e7e6fa86238f613cc124dc794d
jweah555 <j_weah@uncg.edu>
2025-06-16 10:19:38 -0400

    Converted Chat RestAPI to Controller, mostly complete with chat

--- backend-api/src/main/java/com/example/Game_Platform/GameStore/GameStore.java
+++ backend-api/src/main/java/com/example/Game_Platform/GameStore/GameStore.java
@@ -1,4 +1,4 @@
-// package com.example.Game_Platform.GameStore;
+package com.example.Game_Platform.GameStore;
 
  import java.util.List;
 
@@ -9,77 +9,77 @@ import com.example.Game_Platform.Developer.Developer;
  import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 
-// import java.util.List;
+import java.util.List;
 
-// import com.example.Game_Platform.Developer.Developer;
-// import com.example.Game_Platform.Game.Game;
-// import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.OneToMany;
-// import jakarta.persistence.Table;
-
-// @Entity
-// @Table(name = "gameStore")
-// public class GameStore {
-
-//  @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long storeId;
+import com.example.Game_Platform.Developer.Developer;
+import com.example.Game_Platform.Game.Game;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 
-// @OneToMany(mappedBy = "gameStore")
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "gameStore")
+public class GameStore {
+
+ @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long storeId;
+
+
+@OneToMany(mappedBy = "gameStore")
     
-//     @JsonIgnoreProperties("gameStore")
-//     private List<Game> games;
-// @OneToMany(mappedBy = "gameStore")    
-//  @JsonIgnoreProperties("gameStore")   
-//     private List<Developer> developer;
+    @JsonIgnoreProperties("gameStore")
+    private List<Game> games;
+@OneToMany(mappedBy = "gameStore")    
+ @JsonIgnoreProperties("gameStore")   
+    private List<Developer> developer;
 
-// public GameStore(){
+public GameStore(){
 
-// }
-// public GameStore(List<Game> games,List<Developer> developer)
-// {
-//     this.games=games;
-//     this.developer=developer;
+}
+public GameStore(List<Game> games,List<Developer> developer)
+{
+    this.games=games;
+    this.developer=developer;
     
-// }
-// public GameStore(Long storeId, List<Game> games,List<Developer> developer)
-// {
-//     this.games=games;
-//     this.developer=developer;
-//     this.storeId=storeId;
-// }
+}
+public GameStore(Long storeId, List<Game> games,List<Developer> developer)
+{
+    this.games=games;
+    this.developer=developer;
+    this.storeId=storeId;
+}
 
-// public List<Game> getGames(){
-//     return this.games;
-// }
+public List<Game> getGames(){
+    return this.games;
+}
 
-// public List<Developer> getDeveloper(){
-//     return this.developer;
-// }
+public List<Developer> getDeveloper(){
+    return this.developer;
+}
 
-// public void setGames(List<Game> games){
-//     this.games=games;
-// }
+public void setGames(List<Game> games){
+    this.games=games;
+}
 
-// public void setDeveloper(List<Developer> developer){
-//     this.developer=developer;
+public void setDeveloper(List<Developer> developer){
+    this.developer=developer;
 
-// }
+}
 
-// public Long getId(){
-//     return this.storeId;
-// }
+public Long getId(){
+    return this.storeId;
+}
 
-// public void setStoreId(Long storeId){
-//     this.storeId=storeId;
-// }
+public void setStoreId(Long storeId){
+    this.storeId=storeId;
+}
 
 
-// }
+}
